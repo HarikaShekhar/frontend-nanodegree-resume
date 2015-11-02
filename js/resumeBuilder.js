a81edc897e115904c33efcd05db75a317ef97a97
HarikaShekhar <harika.uid@gmail.com>
2015-11-02 15:02:59 -0800

    Feat: Add Projects.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,14 +2,14 @@ var bio = {
 	"name": "Harika Mateti",
 	"role": "Web Developer",
 	"contacts": {
-		"mobile": "(408)673-1384",
+		"mobile": "(408) 673-1384",
 		"email": "harika.uid@gmail.com",
 		"github": "HarikaShekkhar",
 		"twitter": "@harikashekhar",
 		"location": "San Jose"
 	},
 	"welcomeMessage": "Hi, my name is Harika. I love creating beautiful & functional user experiences. As a graduate of Computer Science Technology with 3 years experience working in various roles, I have a wide range of skills. I am very passionate about web development and want to become a Front End Developer who focuses on writing clean, elegant and efficient code. Love HTML, CSS and a touch of jQuery.<br><br> When I am not coding or pushing pixels you will find me painting, sketching, playing Mortal Combat on PS4 or googling around interesting stuff.",
-	"skills": ["HTML5", "CSS3", "Javascript", "jQuery", "Bootstrap", "Shell scripting"],
+	"skills": ["HTML5", "CSS3", "Javascript", "jQuery", "Bootstrap", "Shell scripting", "Shell scripting", "SQL", "PL/SQL"],
 	"bioPic": "images/avatar.jpg",
 	// TODO: Implement display function
 	"display": function() {
@@ -24,8 +24,8 @@ var bio = {
 		var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
 		$("#header").prepend(formattedHeaderRole).prepend(formattedHeaderName);
-		$("#topContacts").append(formattedMobile).append(formattedEmail).append(formattedGithub).append(formattedTwitter).append(formattedLocation);
-		$("#footerContacts").append(formattedEmail).append(formattedGithub).append(formattedMobile);
+		$("#topContacts, #footerContacts, .collapsed-footer").append(formattedMobile).append(formattedEmail).append(formattedGithub).append(formattedTwitter).append(formattedLocation);
+		// $("#footerContacts").append(formattedEmail).append(formattedGithub).append(formattedMobile);
 
 		var skillsLength = bio.skills.length;
 		if ( skillsLength > 0) {
@@ -42,11 +42,12 @@ var bio = {
 // Call display() function to display bio information
 bio.display();
 
+
 var education = {
 	"schools": [
 		{
 			"name": "Jawaharlal Nehru Technological University",
-			"location": "Hyderabad, India",
+			"location": "Hyderabad, Telangana",
 			"degree": "Bachelors",
 			"majors": ["Computer Science Engineering"],
 			"dates": 2009,
@@ -55,7 +56,7 @@ var education = {
 		},
 		{
 			"name": "Nalanda Junior College",
-			"location": "Hyderabad, India",
+			"location": "Hyderabad, Telangana",
 			"degree": "Intermediate",
 			"majors": ["Math", "Physics", "Chemistry"],
 			"dates": 2005,
@@ -142,7 +143,7 @@ var work = {
 			"description": "Bacon ipsum dolor amet turducken ball tip pork tenderloin. Beef ribs biltong tri-tip jowl ground round flank, alcatra pastrami filet mignon rump pork chop bacon picanha. Shoulder landjaeger spare ribs tail capicola, flank chicken short loin pork chop meatloaf andouille sausage shankle filet mignon ribeye. Spare ribs pig alcatra kevin rump shank pastrami jowl boudin turkey strip steak turducken ball tip corned beef flank. Boudin shankle beef ribs andouille. Cupim strip steak beef ribs kevin, frankfurter jowl andouille pork belly brisket."
 		},
 		{
-			"employer": "Swecha",
+			"employer": "Swecha by Harika Shekhar",
 			"title": "Fashion Designer",
 			"dates": "2013 March - 2013 December",
 			"location": "Hyderabad, India",
@@ -172,3 +173,84 @@ var work = {
 };
 
 work.display();
+
+var projects = {
+	"projects": [
+		{
+			// TODO: Add links to title
+			"title": "Udacity : Build a Portfolio Site",
+			"dates": "2015 October",
+			"description": "A responsive website that will display images, descriptions and links to each of the portfolio projects that I will complete throughout the course of the Front-End Web Developer Nanodegree.",
+			"url": "https://github.com/HarikaShekhar/FE-Nanodegree-Project-1",
+			// TODO: Update Images
+			"images": [
+				"images/portfolio-1.png",
+				"images/portfolio-2.png",
+				"images/portfolio-3.png",
+				"images/portfolio-4.png"
+			]
+		},
+		{
+			// TODO: Add links to title
+			"title": "Udacity : About Me",
+			"dates": "2015 October",
+			"description": "About Me demostrates basic HTML and CSS skills.",
+			"url": "https://github.com/HarikaShekhar/FE-Nanodegree-Project-0",
+			// TODO: Update Images
+			"images": [
+				"images/aboutMe-1.png",
+				"images/aboutMe-2.png"
+			]
+		},
+		{
+			// TODO: Add links to title
+			"title": "Capital One : Enterprise Fulfillment IT (EFIT)",
+			"dates": "2011 January - 2013 February",
+			"description": "EFIT is an Outbound application that creates monthly, quarterly and yearly Statements, Offers and Letters for Capital One customers. Outbound applications are critical regulatory applications for Capital One wherein all the customer communications must be delivered within specific days of generation. Outbound applications extract the required data from the upstream Capital One applications and formats into the customer communications using 3rd party formatting Dialog tool. This project involves Design, Development, Maintenance, Enhancement and Production Support.",
+			"url": "https://github.com/HarikaShekhar/FE-Nanodegree-Project-0",
+			// TODO: Update Images
+			"images": [
+				"images/capitalOne-1.png",
+				"images/capitalOne-2.png"
+			]
+		},
+		{
+			// TODO: Add links to title
+			"title": "Charles Schwab : CRT Letters  development, maintenance and Support",
+			"dates": "2010 February - 2010 December",
+			"description": "Client Reporting Technology is a batch application that creates monthly, quarterly and yearly brokerage Letters for Schwab customers. It is hosted on the IBM Mainframe. Letters applications is one of the critical regulatory applications for Schwab wherein all the customer letters must be delivered within 7 days of generation. Letters extract the required data from the upstream Schwab applications and formats into the customer letters using 3rd party formatting tools — DOC1 and Dialogue. This project involves providing Maintenance, Enhancement and Production Support for the statement application. <br><br>It is a multi-tier project. It has different platforms involved such as UNIX/LINUX, Mainframes and different technologies such as JAVA, bash Scripting, COBOL.",
+			"url": "https://github.com/HarikaShekhar/FE-Nanodegree-Project-0",
+			// TODO: Update Images
+			"images": [
+				"images/charlesSchwab-1.png",
+				"images/charlesSchwab-2.png"
+			]
+		}
+	],
+	"display": function() {
+		for(project in projects.projects){
+			$("#projects").append(HTMLprojectStart);
+			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+			formattedProjectTitle = formattedProjectTitle.replace("#", projects.projects[project].url);
+			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+			var formattedProjectDesciption = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+			$(".project-entry:last").append(formattedProjectTitle).append(formattedProjectDates).append(formattedProjectDesciption);
+			if (projects.projects[project].images.length > 0) {
+				for (image in projects.projects[project].images) {
+					var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image])
+					$(".images-section:last").append(formattedProjectImage);
+				}
+			}
+		}
+	}
+}
+
+projects.display();
+
+$("#contacts").click(function(){
+	$("#topContacts").fadeToggle("slow");
+});
+
+$("#skills-btn").click(function(){
+	$("#skills-h3, #skills").fadeToggle("slow");
+});
